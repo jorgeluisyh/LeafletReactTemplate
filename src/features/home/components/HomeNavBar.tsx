e471af008921088e25b0b3ffe203ec9be34f4e56
Julio Christian Cruz <50924377+jcruzfernandez@users.noreply.github.com>
2025-06-13 12:12:20 -0500

    Update Home NavBar

--- src/features/home/components/HomeNavBar.tsx
+++ src/features/home/components/HomeNavBar.tsx
@@ -1,17 +1,37 @@
-import Container from "react-bootstrap/Container";
-import Navbar from "react-bootstrap/Navbar";
-import logoSedapal from "../../../assets/logoSedapal.png";
-import { Button } from "react-bootstrap";
+import imgBackground from "../../../assets/imgBackground.jpg";
 
 export const HomeNavBar = () => {
   return (
-    <Navbar bg="dark" variant="dark" expand="lg" fixed="top">
-      <Container>
-        <Button variant="outline-light" onClick={() => console.log("hola")}>
-          ☰
-        </Button>
-        <Navbar.Brand href="#home">Herramienta de Administración</Navbar.Brand>
-      </Container>
-    </Navbar>
+    <div
+      className="col-12 bg-light d-flex justify-content-between align-items-center p-3"
+      style={{
+        backgroundImage: `url(${imgBackground})`, //'url("https://github.com/mdo.png")',
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        height: "200px", // Puedes ajustar la altura
+        position: "relative",
+      }}
+    >
+      {/* Texto centrado en el fondo */}
+      <div
+        className="position-absolute w-100 text-center"
+        style={{
+          top: "50%",
+          transform: "translateY(-50%)",
+          color: "rgb(255, 255, 255)",
+          textShadow: "2px 2px 4px rgba(0, 0, 0, 0.7)",
+        }}
+      >
+        <h2>Herramienta de Administración</h2>
+      </div>
+      <img
+        src="https://github.com/mdo.png"
+        alt="Logo"
+        width="50"
+        height="50"
+        className="rounded-circle"
+      />
+      <button className="btn btn-danger">Cerrar sesión</button>
+    </div>
   );
 };
